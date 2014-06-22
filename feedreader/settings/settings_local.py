e5ff60733c3cbfabcc5299cb7287a895cd65bac6
j_schn14 <j_schn14@uni-muenster.de>
2014-06-22 23:34:18 +0200

    very first commit of first structure and data

---
+++ feedreader/settings/settings_local.py
@@ -0,0 +1,11 @@
+__author__ = 'j_schn14'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jadfr',
+        'USER': 'postgres',
+        'PASSWORD': 's0melongTestingPassw0rd',
+        'HOST': 'localhost'
+    }
+}
