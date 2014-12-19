678d6ea0eeba4885b1c1e3f32afd4417fb3acd11
jo-soft <ichhabekeineemail@gmx.net>
2014-12-19 20:15:43 +0100

    create task to update feeds.

--- jadfr/apps/userfeeds/tasks.py
+++ jadfr/apps/userfeeds/tasks.py
@@ -1,1 +1,17 @@
-__author__ = 'j_schn14'
+from apps.userfeeds.models import UserFeed
+from celery.canvas import group
+from feedreader.celery import app
+
+
+def load_feeds(user=None):
+    user_feeds = UserFeed.objects.all()
+    if user:
+        user_feeds = user_feeds.filter(user=user)
+    feeds = set(user_feed.feed for user_feed in user_feeds)
+    group(load_feed_task.s(feed) for feed in feeds)
+
+
+@app.task
+def load_feed_task(feed):
+    feed.update()
+
