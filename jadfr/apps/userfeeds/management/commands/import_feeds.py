62bfe7502113a9a3733da64e8acbef1d675543c6
jo-soft <ichhabekeineemail@gmx.net>
2014-12-30 00:19:31 +0100

    refactored feed import

--- jadfr/apps/userfeeds/management/commands/import_feeds.py
+++ jadfr/apps/userfeeds/management/commands/import_feeds.py
@@ -1,8 +1,5 @@
-from apps.usercategories.models import Category
-from apps.userfeeds.models import UserFeed
+from apps.userfeeds.services import CategoryInfo, FeedInfo, FeedWriteService
 from django.contrib.auth.models import User
-from djangofeeds.models import Feed
-from djangofeeds.models import Category as BaseFeedCategory
 from django.core.management.base import BaseCommand
 from optparse import make_option
 
@@ -29,22 +26,36 @@ class Command(BaseCommand):
         """
         reads feed information from opml file and stores them in the database
         """
-        self.verbose = options.get('verbose')
+        verbose = options.get('verbose')
         user = options.get('user')
         file_name = options.get('filename')
         dry_run = options.get('dry_run')
 
         # if a user is give, select it from the db
-        self.set_user(user)
+        worker = Worker(user_name=user,
+                        file_name=file_name,
+                        verbose=verbose,
+                        dry_run=dry_run)
+        worker.execute()
+
+
+class Worker(object):
+
+    def __init__(self, user_name,file_name, dry_run=False, verbose=False):
+        self.dry_run = dry_run
+        self.file_name = file_name
+        self.verbose = verbose
+        if user_name:
+            self.user = User.objects.get(username=user_name)
+        self.service_class = FeedWriteService(dry_run=self.dry_run, logger=logger)
+
+    def execute(self):
         # read the data from file
-        data = self.parse_file(file_name)
+        data = self._parse_file(self.file_name)
         # store it to the databaase
-        self.save(data, dry_run=dry_run)
+        self.service_class.rsave(data=data)
 
-    def set_user(self, user):
-        self.user = User.objects.get(username=user)
-
-    def parse_file(self, file_name):
+    def _parse_file(self, file_name):
         """
         creates nested structure of feedInfos and feedcategories from the ompl file
         :param file_name: the file to pare
@@ -73,95 +84,3 @@ class Command(BaseCommand):
             else:
                 result.add(self.get_feeds_from_outline(o))
         return result
-
-    def save(self, data, dry_run=False):
-        """
-        saves the Information from a FeedInfo object to the database
-        :param data: Items to save FeedInfoObject
-        :param dry_run: if false, no db operations is performed
-        """
-        for item in data:
-            print "Saving %s" % item
-            if isinstance(item, FeedInfo):
-                save_func = self._save_feed
-            else:
-                save_func = self._save_category
-            if self.verbose:
-                logger.debug('Saving %s' % item)
-            save_func(item, dry_run=dry_run)
-
-    def _save_feed(self, feed_item, dry_run):
-        try:
-            feed = Feed.objects.get(feed_url=feed_item.feed_url)
-        except Feed.DoesNotExist:
-            feed = Feed(
-                feed_url=feed_item.feed_url,
-                name=feed_item.title
-            )
-            if not dry_run:
-                feed.save()
-                feed.categories.add(BaseFeedCategory.objects.get(name=UserFeed.default_base_feed_category_name))
-        try:
-            user_feed = UserFeed.objects.get(feed=feed, user=self.user)
-        except UserFeed.DoesNotExist:
-            user_feed = UserFeed.objects.create(
-                feed=feed,
-                user=self.user
-            )
-        feed_category = Category.objects.get(name=feed_item.category.name)
-        user_feed.categories.add(feed_category)
-        if not dry_run:
-            user_feed.save()
-
-    def _save_category(self, category_item, dry_run):
-        if not Category.objects.filter(name=category_item.name).exists():
-            category = Category(name=category_item.name)
-            if not dry_run:
-                category.save()
-        self.save(category_item)
-
-
-class FeedInfo(object):
-    def __init__(self,
-                 feed_type,
-                feed_url,
-                html_url,
-                title,
-                category
-    ):
-        self.feed_type = feed_type
-        self.feed_url = feed_url
-        self.html_url = html_url
-        self.title = title
-        self.category = category
-
-    def __str__(self):
-        return self.title
-
-
-class CategoryInfo(object):
-    class CategoryInfoIter(object):
-
-        def __init__(self, category_info):
-            self._items = category_info._items
-
-        def next(self):
-            if not hasattr(self, '_iter'):
-                self._iter = iter(self._items)
-            return self._iter.next()
-
-    def __init__(self, name, items=[]):
-        super(CategoryInfo, self).__init__()
-        self.name = name
-        self._items = set()
-        for item in items:
-            self._items.add(item)
-
-    def __iter__(self):
-        return CategoryInfo.CategoryInfoIter(self)
-
-    def add(self, item):
-        self._items.add(item)
-
-    def __str__(self):
-        return self.name
--- jadfr/apps/userfeeds/services.py
+++ jadfr/apps/userfeeds/services.py
@@ -1,1 +1,104 @@
-__author__ = 'j_schn14'
+from apps.usercategories.models import Category
+from apps.userfeeds.models import UserFeed
+
+import logging
+from djangofeeds.models import Feed
+from djangofeeds.models import Category as BaseFeedCategory
+
+
+class FeedWriteService(object):
+
+    def __init__(self, logger=None, dry_run=False):
+        self.logger = logger or logging.getLogger(__name__)
+        self.dry_run = dry_run
+
+    def rsave(self, data):
+        """
+        saves the Information of a FeedInfo object recursively  to the database
+        :param data: Items to save FeedInfoObject
+        :param dry_run: if false, no db operations is performed
+        """
+        for item in data:
+            if isinstance(item, FeedInfo):
+                save_func = self.save_feed
+            else:
+                save_func = self.save_category
+            if self.verbose:
+                self.logger.info("Saving %s", item)
+            save_func(item, dry_run=self.dry_run)
+
+    def save_feed(self, feed_item):
+        try:
+            feed = Feed.objects.get(feed_url=feed_item.feed_url)
+        except Feed.DoesNotExist:
+            feed = Feed(
+                feed_url=feed_item.feed_url,
+                name=feed_item.title
+            )
+            if not self.dry_run:
+                feed.save()
+                feed.categories.add(BaseFeedCategory.objects.get(name=UserFeed.default_base_feed_category_name))
+        try:
+            user_feed = UserFeed.objects.get(feed=feed, user=self.user)
+        except UserFeed.DoesNotExist:
+            user_feed = UserFeed.objects.create(
+                feed=feed,
+                user=self.user
+            )
+        feed_category = Category.objects.get(name=feed_item.category.name)
+        user_feed.categories.add(feed_category)
+        if not self.dry_run:
+            user_feed.save()
+
+    def save_category(self, category_item):
+        if not Category.objects.filter(name=category_item.name).exists():
+            category = Category(name=category_item.name)
+            if not self.dry_run:
+                category.save()
+        self.save(category_item)
+
+
+class FeedInfo(object):
+    def __init__(self,
+                 feed_type,
+                feed_url,
+                html_url,
+                title,
+                category
+    ):
+        self.feed_type = feed_type
+        self.feed_url = feed_url
+        self.html_url = html_url
+        self.title = title
+        self.category = category
+
+    def __str__(self):
+        return self.title
+
+
+class CategoryInfo(object):
+    class CategoryInfoIter(object):
+
+        def __init__(self, category_info):
+            self._items = category_info._items
+
+        def next(self):
+            if not hasattr(self, '_iter'):
+                self._iter = iter(self._items)
+            return self._iter.next()
+
+    def __init__(self, name, items=[]):
+        super(CategoryInfo, self).__init__()
+        self.name = name
+        self._items = set()
+        for item in items:
+            self._items.add(item)
+
+    def __iter__(self):
+        return CategoryInfo.CategoryInfoIter(self)
+
+    def add(self, item):
+        self._items.add(item)
+
+    def __str__(self):
+        return self.name
