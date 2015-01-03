2198e47a566211f9567c29c3da81208ffaf8e272
jo-soft <ichhabekeineemail@gmx.net>
2015-01-04 00:13:59 +0100

    added new-feeds view

--- jadfr/api/feeds/urls.py
+++ jadfr/api/feeds/urls.py
@@ -1,9 +1,16 @@
-from api.feeds.views import FeedList, FeedEntriesList, FeedEntry
+from api.feeds.views import FeedList, FeedEntriesList, FeedEntry, AddFeed
+from apps.userfeeds.models import UserFeedEntry
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     '',
-    url(r'^$', FeedList.as_view()),
-    url(r'^(?P<pk>\d)/$', FeedEntriesList.as_view()),
+    url(r'^$', FeedList.as_view(), name='FeedList'),
+    url(r'^new/$', FeedEntriesList.as_view(),
+        {'filters': {
+            'status__lte': UserFeedEntry.ENTRY_UNREAD_VAL}
+         },
+        name='FeedList'),
+    url(r'^(?P<pk>\d)/$', FeedEntriesList.as_view(), name='BaseFeed'),
     url(r'^(?P<feed_pk>\d+)/(?P<entry_pk>\d+)/$', FeedEntry.as_view(), name='BaseFeedEntry'),
+    url(r'^add/', AddFeed.as_view()),
 )
--- jadfr/api/feeds/views.py
+++ jadfr/api/feeds/views.py
@@ -1,9 +1,20 @@
 from api.feeds.serializers import UserFeedSerializer, UserFeedEntrySerializer
 from apps.userfeeds.models import UserFeed, UserFeedEntry
+from apps.userfeeds import tasks
+
+from django.core.urlresolvers import reverse
+from logging import getLogger
+from rest_framework import status
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+logger = getLogger(__name__)
 
 
 class FeedList(ListAPIView):
+
     queryset = UserFeed.objects.all()
     serializer_class = UserFeedSerializer
 
@@ -13,18 +24,22 @@ class FeedList(ListAPIView):
 
 
 class FeedEntriesList(ListAPIView):
-    queryset = UserFeedEntry.objects.all()
+    queryset = UserFeedEntry.objects.all()(tasks.inc_entry_status)
     serializer_class = UserFeedEntrySerializer
 
     def get_queryset(self):
-        feed_pk = self.kwargs['pk']
-        user = self.request.user
-        return self.queryset.filter(feed_id=feed_pk, feed__user=user)
+        filters = self.kwargs.get('filters', {})
+        feed_pk = self.kwargs.get('pk', None)
+        if feed_pk:
+            filters['feed_id'] = feed_pk
+        filters['feed__user'] = self.request.user
+        res = self.queryset.filter(**filters)
+        return res
 
 
 class FeedEntry(RetrieveAPIView):
 
-    queryset = UserFeedEntry.objects.all()
+    queryset = UserFeedEntry.objects.all()(tasks.inc_entry_status)
     serializer_class = UserFeedEntrySerializer
 
     def get_queryset(self):
@@ -32,7 +47,28 @@ class FeedEntry(RetrieveAPIView):
         return self.queryset.filter(feed__user=user)
 
     def get_object(self):
-        feed_pk  = self.kwargs['feed_pk']
+        feed_pk = self.kwargs['feed_pk']
         entry_pk = self.kwargs['entry_pk']
         obj = self.get_queryset().get(pk=entry_pk, feed_id=feed_pk)
         return obj
+
+
+class AddFeed(APIView):
+
+    def post(self, request, *args, **kwargs):
+        feed_urls = [self.request.DATA.get('url', None)]
+        if not feed_urls:
+            feed_urls = self.request.DATA['urls']
+        user = self.request.user
+        tasks.add_feeds(user, feed_urls)
+        headers = self.get_success_headers(feed_urls)
+        return Response(feed_urls, status=status.HTTP_201_CREATED, headers=headers)
+
+    def get_success_headers(self, data):
+        # I don't like this, because it's not RESTFull, but I don't know how to
+        # to manage it, because ot cam add more than one feed and it is a celery task in
+        try:
+            base_url = reverse('FeedList')
+            return {'Location': base_url}
+        except (TypeError, KeyError):
+            return {}
