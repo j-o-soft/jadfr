b56babb564ef316c019fad689a8d4136818becbb
jo-soft <ichhabekeineemail@gmx.net>
2015-01-31 23:37:58 +0100

    for some reasons i killed on line in the last commit - now fixed

--- jadfr/api/feeds/tests/unittests/test_serializers.py
+++ jadfr/api/feeds/tests/unittests/test_serializers.py
@@ -56,3 +56,4 @@ class TestProxyModelSerializer(TestCase):
         stream = BytesIO(json)
         data = JSONParser().parse(stream)
         for field_name, value in data.iteritems():
+            assert getattr(m1, field_name) == value
